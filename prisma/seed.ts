1ad77cdf921aa9b255308645c22faca42c9cb2ab
LeviMaycon <levioficial361@gmail.com>
2025-03-01 08:23:19 -0300

    feat: create initial seed for populating database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,10 @@
-import { PrismaClient, Speciality } from '@prisma/client';
+import { PrismaClient, Speciality, DaysOpen } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // Criando um Admin
-  const admin = await prisma.admin.create({
+  // Criando o primeiro Admin
+  const admin1 = await prisma.admin.create({
     data: {
       fullname: 'João Silva',
       phone: '123456789',
@@ -26,22 +26,63 @@ async function main() {
           {
             cnpj: '12345678000199',
             state_registration: '1234567890',
-            specialty: [Speciality.PIZZARIA, Speciality.BRASILEIRA],
+            speciality: [Speciality.PIZZARIA, Speciality.BRASILEIRA],
+            name: 'Xgrilo',
+            logo: 'logo.jpg',
+            isOpen: true,
+            BusinessHours: {
+              create: [
+                {
+                  day: DaysOpen.MONDAY,
+                  openHour: '07:00',
+                  closeHour: '21:00',
+                },
+                {
+                  day: DaysOpen.TUESDAY,
+                  openHour: '07:00',
+                  closeHour: '21:00',
+                },
+                {
+                  day: DaysOpen.WEDNESDAY,
+                  openHour: '07:00',
+                  closeHour: '21:00',
+                },
+                {
+                  day: DaysOpen.THURSDAY,
+                  openHour: '07:00',
+                  closeHour: '21:00',
+                },
+                {
+                  day: DaysOpen.FRIDAY,
+                  openHour: '07:00',
+                  closeHour: '22:00',
+                },
+                {
+                  day: DaysOpen.SATURDAY,
+                  openHour: '07:00',
+                  closeHour: '22:00',
+                },
+                {
+                  day: DaysOpen.SUNDAY,
+                  openHour: '07:00',
+                  closeHour: '22:00',
+                },
+              ],
+            },
           },
         ],
       },
     },
   });
 
-  console.log({ admin });
+  console.log({ admin1 });
 
-  // Criando outro Admin
   const admin2 = await prisma.admin.create({
     data: {
       fullname: 'Maria Souza',
       phone: '987654321',
       email: 'maria.souza@email.com',
-      password: 'hashedpassword', // Aqui você deve usar um hash de senha
+      password: 'hashedpassword',
       Adress: {
         create: [
           {
@@ -58,7 +99,39 @@ async function main() {
           {
             cnpj: '98765432000100',
             state_registration: '9876543210',
-            specialty: [Speciality.HAMBURGUERIA, Speciality.FAST_FOOD],
+            speciality: [Speciality.HAMBURGUERIA, Speciality.FAST_FOOD],
+            name: 'Xtuto Gabriel',
+            logo: 'logo2.jpg',
+            isOpen: true,
+            BusinessHours: {
+              create: [
+                {
+                  day: DaysOpen.MONDAY,
+                  openHour: '08:00',
+                  closeHour: '22:00',
+                },
+                {
+                  day: DaysOpen.TUESDAY,
+                  openHour: '08:00',
+                  closeHour: '22:00',
+                },
+                {
+                  day: DaysOpen.WEDNESDAY,
+                  openHour: '08:00',
+                  closeHour: '22:00',
+                },
+                {
+                  day: DaysOpen.THURSDAY,
+                  openHour: '08:00',
+                  closeHour: '22:00',
+                },
+                {
+                  day: DaysOpen.FRIDAY,
+                  openHour: '08:00',
+                  closeHour: '22:00',
+                },
+              ],
+            },
           },
         ],
       },
